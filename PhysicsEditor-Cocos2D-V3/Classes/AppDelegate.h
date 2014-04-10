2c8906bbd142af641e3409cf2cd18c420c8493fc
Joachim Grill <joachim@codeandweb.com>
2014-04-10 11:45:14 +0200

    new project based on ‘cocos2d iOS’ template (Cocos2D V3.0.0 rc4)

---
+++ PhysicsEditor-Cocos2D-V3/Classes/AppDelegate.h
@@ -0,0 +1,13 @@
+//
+//  AppDelegate.h
+//  PhysicsEditor-Cocos2D-V3
+//
+//  Created by Joachim Grill on 10.04.14.
+//  Copyright CodeAndWeb GmbH 2014. All rights reserved.
+//
+// -----------------------------------------------------------------------
+
+#import "cocos2d.h"
+
+@interface AppDelegate : CCAppDelegate
+@end
---
+++ PhysicsEditor-Cocos2D-V3/Supporting Files/main.m
@@ -0,0 +1,17 @@
+//
+//  main.m
+//  PhysicsEditor-Cocos2D-V3
+//
+//  Created by Joachim Grill on 10.04.14.
+//  Copyright CodeAndWeb GmbH 2014. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+int main(int argc, char *argv[]) {
+    
+    @autoreleasepool {
+        int retVal = UIApplicationMain(argc, argv, nil, @"AppDelegate");
+        return retVal;
+    }
+}
